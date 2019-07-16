df61ba4522176b91192f8d19516c074f9b69efcd
Michal <michalr11@gmail.com>
2019-07-16 19:09:54 +0200

    Tests 1

--- src/test/java/pl/devfoundry/testing/AccountTest.java
+++ src/test/java/pl/devfoundry/testing/AccountTest.java
@@ -4,11 +4,20 @@ import org.junit.jupiter.api.Test;
 import testing.Account;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AccountTest {
     @Test
     void myTest() {
         Account newAccount = new Account();
+        assertFalse(newAccount.isActive(), " Check if new account in not active");
+    }
+
+    @Test
+    void myTest2() {
+        Account newAccount = new Account();
         assertFalse(newAccount.isActive());
+        newAccount.activate();
+        assertTrue(newAccount.isActive());
     }
 }
