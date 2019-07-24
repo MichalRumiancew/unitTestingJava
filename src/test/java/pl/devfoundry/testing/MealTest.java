70da85ae3de4e6b6acc65a5e9385d8547acd47b1
Michal <michalr11@gmail.com>
2019-07-24 13:13:04 +0200

    Tests to Collectiions pl: testy do testowania kolekcji

--- src/test/java/pl/devfoundry/testing/MealTest.java
+++ src/test/java/pl/devfoundry/testing/MealTest.java
@@ -2,6 +2,10 @@ package pl.devfoundry.testing;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MealTest {
@@ -15,6 +19,7 @@ class MealTest {
         int dicountedPrice = meal.getDiscountedPrice(7);
         //then
         assertEquals(28, dicountedPrice);
+        assertThat(dicountedPrice, equalTo(28));
     }
 
     @Test
@@ -25,6 +30,7 @@ class MealTest {
         Meal meal2 = meal1;
         //then
         assertSame(meal1, meal2);
+        assertThat(meal1, sameInstance(meal2));
 
     }
 
@@ -36,6 +42,7 @@ class MealTest {
         Meal meal2 = new Meal(20);
         //then
         assertNotSame(meal1, meal2);
+        assertThat(meal1, not(sameInstance(meal2)));
 
     }
 
