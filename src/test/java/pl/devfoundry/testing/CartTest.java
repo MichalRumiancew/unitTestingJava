857397ca6f7dfcb284ea742f7cca7ad9e6ac113e
Michal <michalr11@gmail.com>
2019-07-25 13:06:30 +0200

    @DisplayName @Disable

--- src/test/java/pl/devfoundry/testing/CartTest.java
+++ src/test/java/pl/devfoundry/testing/CartTest.java
@@ -1,14 +1,17 @@
 package pl.devfoundry.testing;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName(" Test casec for Cart")
 class CartTest {
 
     @Test
+    @DisplayName("Cart is able to process 1000 orders in 100 ms")
     void simulateLargeOrder() {
         //given
         Cart cart = new Cart();
