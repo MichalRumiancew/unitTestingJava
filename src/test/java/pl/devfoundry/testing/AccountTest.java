8326ccf57e8deb045822d7754bc5b1ecac1ca43b
Michal <michalr11@gmail.com>
2020-04-14 10:21:29 +0200

    @ test is = assert Junit5

--- src/test/java/pl/devfoundry/testing/AccountTest.java
+++ src/test/java/pl/devfoundry/testing/AccountTest.java
@@ -20,70 +20,75 @@ class AccountTest {
 
         //then
         assertFalse(newAccount.isActive());
-        assertThat(newAccount.isActive(), equalTo(false));
-        assertThat(newAccount.isActive(), is(false));
+//        if (!newAccount.isActive()) {
+//            return "Ok";
+//        } else {
+//            throw new IllegalStateException();
+
+            assertThat(newAccount.isActive(), equalTo(false));
+            assertThat(newAccount.isActive(), is(false));
+        }
+
+        @Test
+        void newAccountShouldNotBeActiveAfterCreation () {
+            //given
+            //when
+            Account newAccount = new Account();
+            //then
+            assertFalse(newAccount.isActive(), " Check if new account in not active");
+        }
+
+        @Test
+        void accountShouldBeActiveAfterActivation () {
+            //given
+            Account newAccount = new Account();
+            //when
+            newAccount.activate();
+            //then
+            assertTrue(newAccount.isActive());
+        }
+
+        @Test
+        void newlyCreatedAccountShouldNotHaveDefaultDeliveryAddressSet () {
+
+            //given
+            Account account = new Account();
+            //when
+            Address address = account.getDefaultDeliveryAddres();
+            //then
+            assertNull(address);
+
+        }
+
+        @Test
+        void defaultDeliveryAddressShouldNotBeNullAfterBeingSet () {
+            //given
+            Address address = new Address("Krakowska", "67c");
+            Account account = new Account();
+            account.setDefaultDeliveryAddres(address);
+
+            //when
+            Address defaultAddress = account.getDefaultDeliveryAddres();
+
+            // then
+            assertNotNull(defaultAddress);
+
+            //assertThat()
+
+        }
+
+        @RepeatedTest(5)
+        void newAccountWithNotNullAddressShouldBeActive () {
+
+            //given
+            Address address = new Address("Puławska", "46/6");
+
+            //when
+            Account account = new Account(address);
+
+            //then
+            assumingThat(address != null, () -> {
+                assertTrue(account.isActive());
+            });
+        }
     }
-
-    @Test
-    void newAccountShouldNotBeActiveAfterCreation() {
-        //given
-        //when
-        Account newAccount = new Account();
-        //then
-        assertFalse(newAccount.isActive(), " Check if new account in not active");
-    }
-
-    @Test
-    void accountShouldBeActiveAfterActivation() {
-        //given
-        Account newAccount = new Account();
-        //when
-        newAccount.activate();
-        //then
-        assertTrue(newAccount.isActive());
-    }
-
-    @Test
-    void newlyCreatedAccountShouldNotHaveDefaultDeliveryAddressSet() {
-
-        //given
-        Account account = new Account();
-        //when
-        Address address = account.getDefaultDeliveryAddres();
-        //then
-        assertNull(address);
-
-    }
-
-    @Test
-    void defaultDeliveryAddressShouldNotBeNullAfterBeingSet() {
-        //given
-        Address address = new Address("Krakowska", "67c");
-        Account account = new Account();
-        account.setDefaultDeliveryAddres(address);
-
-        //when
-        Address defaultAddress = account.getDefaultDeliveryAddres();
-
-        // then
-        assertNotNull(defaultAddress);
-
-        //assertThat()
-
-    }
-
-    @RepeatedTest(5)
-    void newAccountWithNotNullAddressShouldBeActive(){
-
-        //given
-        Address address = new Address("Puławska", "46/6");
-
-        //when
-        Account account = new Account(address);
-
-        //then
-        assumingThat(address !=null, () ->{
-            assertTrue(account.isActive());
-        });
-    }
-}
